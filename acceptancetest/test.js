c0b96b5cfae5d035f9936432cc1499197129358c
Ludwig Magnusson <ludwig@mediatool.com>
2022-12-09 10:26:44 +0100

    handle default exports

--- acceptancetest/test.js
+++ acceptancetest/test.js
@@ -1,9 +1,10 @@
 import { equal } from 'assert'
 import { replace, reset } from '../lib/replace.js'
 
+await replace('pad', { default: (string) => `${string} padded` })
 await replace('./stub-me.js', { foo: () => 'bar' })
 const { main: withStub } = await import('./file-under-test.js')
-equal(withStub, 'processed bar--')
+equal(withStub, 'processed bar padded')
 reset()
 // const { main: withoutStub } = await import('./file-under-test.js')
 // equal(withoutStub, 'processed foo')
