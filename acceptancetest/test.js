73978606c6ba475c0a0bd80d673d0ebbea574176
Ludwig Magnusson <ludwig@mediatool.com>
2024-10-28 14:00:29 +0100

    fix: make reset work
    This makes reset work properly. After a reset, no special urls should be
    generated by resolve. The counts still needs to be kept since if a
    replaced file is reset and replaced again, the count should not be the
    same as with the first replace.
    
    To solve this, arrays listing the urls of files currently under
    replacement was added to the info. If a file is not in this array, no
    count will be generated.

--- acceptancetest/test.js
+++ acceptancetest/test.js
@@ -28,6 +28,6 @@ equal(
 )
 
 reset()
-// const { main: withoutStub } = await import('./file-under-test.js')
-// equal(withoutStub, 'processed foo')
+const { main: withoutStub } = await import('./file-under-test.js')
+equal(withoutStub, 'processed foo--', 'Replacements were not reset')
 process.stdout.write('\x1B[32mAcceptance test passed\x1B[0m')
